d376417c7182265fcaa05a6f1421433fac42d9ab
Artos Web <teodor.at.tod@gmail.com>
2022-07-14 16:17:02 +0300

    edit homeComponent

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -50,7 +50,7 @@ const Home = () => {
         <div className="container pt-5 pb-3">
             <div className="text-center mb-5">
                 <h5 className="text-primary text-uppercase mb-3" >Subjects</h5>
-                <h1>Explore Top Subjects</h1>
+                <h1>Explore Top Courses</h1>
             </div>
             <div className="row">
                 <div className="col-lg-3 col-md-6 mb-4">
@@ -89,42 +89,7 @@ const Home = () => {
                         </a>
                     </div>
                 </div>
-                <div className="col-lg-3 col-md-6 mb-4">
-                    <div className="cat-item position-relative overflow-hidden rounded mb-2">
-                        <img className="img-fluid" src="img/cat-5.jpg" alt="" />
-                        <a className="cat-overlay text-white text-decoration-none" href="">
-                            <h4 className="text-white font-weight-medium">Marketing</h4>
-                            <span>100 Courses</span>
-                        </a>
-                    </div>
-                </div>
-                <div className="col-lg-3 col-md-6 mb-4">
-                    <div className="cat-item position-relative overflow-hidden rounded mb-2">
-                        <img className="img-fluid" src="img/cat-6.jpg" alt="" />
-                        <a className="cat-overlay text-white text-decoration-none" href="">
-                            <h4 className="text-white font-weight-medium">Research</h4>
-                            <span>100 Courses</span>
-                        </a>
-                    </div>
-                </div>
-                <div className="col-lg-3 col-md-6 mb-4">
-                    <div className="cat-item position-relative overflow-hidden rounded mb-2">
-                        <img className="img-fluid" src="img/cat-7.jpg" alt="" />
-                        <a className="cat-overlay text-white text-decoration-none" href="">
-                            <h4 className="text-white font-weight-medium">Content Writing</h4>
-                            <span>100 Courses</span>
-                        </a>
-                    </div>
-                </div>
-                <div className="col-lg-3 col-md-6 mb-4">
-                    <div className="cat-item position-relative overflow-hidden rounded mb-2">
-                        <img className="img-fluid" src="img/cat-8.jpg" alt="" />
-                        <a className="cat-overlay text-white text-decoration-none" href="">
-                            <h4 className="text-white font-weight-medium">SEO</h4>
-                            <span>100 Courses</span>
-                        </a>
-                    </div>
-                </div>
+                
             </div>
         </div>
     </div>
