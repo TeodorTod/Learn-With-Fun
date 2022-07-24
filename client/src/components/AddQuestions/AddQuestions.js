8860311dda9088bd0977913fbff4ebdcfc2af525
Artos Web <teodor.at.tod@gmail.com>
2022-07-24 22:17:23 +0300

    add server

---
+++ client/src/components/AddQuestions/AddQuestions.js
@@ -0,0 +1,45 @@
+import './AddQuestions.css';
+
+const AddQuestions = () => {
+
+    return(
+        <div className="container-fluid py-5">
+        <div className="container py-5">
+            <div className="text-center mb-5">
+                <h5 className="text-primary text-uppercase mb-3"></h5>
+                <h1>Make a question to a teacher</h1>
+            </div>
+            <div className="row justify-content-center">
+                <div className="col-lg-8">
+                    <div className="contact-form bg-secondary rounded p-5">
+                        <div id="success"></div>
+                        <form name="sentMessage" id="contactForm" noValidate="novalidate">
+                            <div className="control-group">
+                                <input type="text" className="form-control border-0 p-4" id="name" placeholder="Your Name" required="yes" data-validation-required-message="Please enter your name" />
+                                <p className="help-block text-danger"></p>
+                            </div>
+                            <div className="control-group">
+                                <input type="text" className="form-control border-0 p-4" id="subject" placeholder="Subject" required="required" data-validation-required-message="Please enter a subject" />
+                                <p className="help-block text-danger"></p>
+                            </div>
+                            <div className="control-group">
+                                <input type="email" className="form-control border-0 p-4" id="email" placeholder="Image Url" required="required" data-validation-required-message="Please enter your email" />
+                                <p className="help-block text-danger"></p>
+                            </div>
+                            <div className="control-group">
+                                <textarea className="form-control border-0 py-3 px-4" rows="5" id="message" placeholder="Message" required="required" data-validation-required-message="Please enter your message"></textarea>
+                                <p className="help-block text-danger"></p>
+                            </div>
+                            <div className="text-center">
+                            <button classNameName="btn btn-primary py-md-2 px-md-4 font-weight-semi-bold mt-2">Send</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    );
+}
+
+export default AddQuestions;
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,59 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../contexts/AuthContext";
+import "./Header.css";
+
+const Header = () => {
+
+    const { user } = useContext(AuthContext);
+
+    let guestNavigation = (
+        <div className="navbar-nav py-0">
+            <Link to="/login" className="nav-item nav-link">Login</Link>
+            <Link to="/register" className="nav-item nav-link">Register</Link>
+        </div>
+
+    );
+
+    let userNavigation = (
+
+        <div className="col-lg-9">
+            <div className="navbar-nav py-0">
+                <Link to="/my-courses" className="nav-item nav-link">My Courses</Link>
+                <Link to="/add-questions" className="nav-item nav-link">Add Questions</Link>
+                <Link to="/logout" className="nav-item nav-link">Logout</Link>
+            </div>
+        </div>
+
+    );
+
+    return (
+        <div className="col-lg-9" >
+            <nav id="navbar" className="navbar navbar-expand-lg bg-light navbar-light py-3 py-lg-0 px-0">
+                <a href="" className="text-decoration-none d-block d-lg-none">
+                    <h1 className="m-0"><span className="text-primary">E</span>COURSES</h1>
+                </a>
+                <div className="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                    <div className="navbar-nav py-0">
+                        <Link to="/" className="text-decoration-none">
+                            <h1 className="m-0"><span className="text-primary">LEARN</span>withFun</h1>
+                        </Link>
+                        <Link to="/" className="nav-item nav-link active">Home</Link>
+                        <Link to="/about" className="nav-item nav-link">About</Link>
+                        <Link to="/courses" className="nav-item nav-link">Courses</Link>
+                        <Link to="/teachers" className="nav-item nav-link">Teachers</Link>
+                        <Link to="/contact" className="nav-item nav-link">Contact</Link>
+                    </div>
+
+                    {user.email
+                        ? userNavigation
+                        : guestNavigation
+                    }
+
+                </div>
+            </nav>
+        </div>
+    );
+}
+
+export default Header;
