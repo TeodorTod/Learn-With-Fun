1947acef433069a16ab26274e04cbc19e72d4f43
Artos Web <teodor.at.tod@gmail.com>
2022-08-09 15:33:17 +0300

    edit footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import "./Footer.css";
 
 const Footer = () => {
@@ -6,7 +7,7 @@ const Footer = () => {
         <div className="container-fluid bg-dark text-white border-top py-4 px-sm-3 px-md-5">
         <div className="row">
             <div className="col-lg-6 text-center text-md-left mb-3 mb-md-0">
-                <p className="m-0 text-white">&copy; <a href="#">LEARNwithFun</a>. All Rights Reserved. Designed by <a href="https://htmlcodex.com">HTML Codex</a>
+                <p className="m-0 text-white">&copy; <Link to="/">LEARNwithFun</Link>. All Rights Reserved. Designed by <a href="https://htmlcodex.com">HTML Codex</a>
                 </p>
             </div>
         </div>
